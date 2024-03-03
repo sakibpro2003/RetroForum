4607c6b3d34d6132811d0496ea1ad496c5ff932d
sakibpro2003 <sakibprodhan2003@gmail.com>
2024-03-03 15:30:34 +0600

    view count added

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
-const postCardContainer = document.getElementById('postCardContainer');
-const createSpinner = document.createElement('div');
-async function  fetchData() {
-  createSpinner.innerHTML = `<span class="loading loading-dots loading-lg"></span>`
-  const response = await fetch("https://openapi.programming-hero.com/api/retro-forum/posts");
+const postCardContainer = document.getElementById("postCardContainer");
+const postViewContainer = document.getElementById("postViewContainer");
+
+async function fetchData() {
+  const response = await fetch(
+    "https://openapi.programming-hero.com/api/retro-forum/posts"
+  );
   const data = await response.json();
   console.log(data.posts);
   showPost(data.posts);
-  if(data.posts.length > 0){
-
-  }
 }
 
+const addToRead = (postName, view_count) => {
+  // console.log(postName , view_count);
+  const createNew = document.createElement("div");
+  // postViewContainer.classList.add('bg-white rounded-lg gap-10 flex p-4');
+  createNew.innerHTML = `
+<div class=" bg-white rounded-lg gap-10 flex p-4 mt-4">
+                            <p>${postName}</p>
+                            <div class="flex gap-1 items-center">
+                                <i class="fa-regular fa-eye"></i>
+                                <p>${view_count}</p>
+                            </div>
+                        </div>
+`;
+  postViewContainer.appendChild(createNew);
+};
 
 fetchData();
-const showPost = (data)=>{
-  data.forEach(element => {
-    const cardCreate = document.createElement('div');
-    cardCreate.innerHTML =`
+const showPost = (data) => {
+  data.forEach((element) => {
+    const cardCreate = document.createElement("div");
+    cardCreate.innerHTML = `
     <div class="">
                         <div class=" p-2 lg:p-6 card card-side bg-base-100 shadow-xl flex flex-col lg:flex-row">
                             <figure><img class="w-20 rounded-md"
@@ -48,7 +62,7 @@ const showPost = (data)=>{
                                     </div>
                                     <div class="card-actions justify-end ">
 
-                                        <button class="btn  rounded-full bg-green-500 border-none "><i
+                                        <button onclick="addToRead('${element.title}','${element.view_count}')" class="btn  rounded-full bg-green-500 border-none "><i
                                                 class="fa-regular fa-envelope-open text-white"></i>
                                         </button>
 
@@ -57,9 +71,7 @@ const showPost = (data)=>{
                             </div>
                         </div>
                     </div>
-    `
+    `;
     postCardContainer.appendChild(cardCreate);
-
-    // console.log(element.title);
   });
-}
+};
